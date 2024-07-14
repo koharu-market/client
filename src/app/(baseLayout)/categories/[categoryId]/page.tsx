8142e05901b5da882436c9feba8189bfc9452506
seokmin <msm0748@gmail.com>
2024-07-14 17:49:06 +0900

    refactor: main scss 폴더 이동 및 container 클래스 css 수정

--- src/app/(baseLayout)/categories/[categoryId]/page.tsx
+++ src/app/(baseLayout)/categories/[categoryId]/page.tsx
@@ -3,7 +3,7 @@ import { products } from '@/data/products';
 
 export default function Page() {
   return (
-    <div className="container mt-10">
+    <div className="container">
       <div className="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
         {products.map((product) => (
           <Card key={product.id} product={product} />
