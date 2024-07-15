bad3e53f139834b53f31a62b01ebc15c9b53f8ea
seokmin <msm0748@gmail.com>
2024-07-15 14:30:49 +0900

    refactor: header 장바구니 아이콘 수정

--- src/components/layout/Header/RightNavbar.tsx
+++ src/components/layout/Header/RightNavbar.tsx
@@ -15,13 +15,13 @@ export default function RightNavbar() {
             <IoMdSearch />
           </button>
         </div>
-        <div className="relative">
-          <Link href="/cart" className="md:text-3xl text-2xl flex justify-center items-center">
+        <div>
+          <Link href="/cart" className="md:text-3xl text-2xl flex justify-center items-center relative">
             <BsCart2 />
+            <div className="flex items-center justify-center bg-[#ef7014] px-1 min-w-[16px] h-4 absolute rounded-full -top-1 -right-1 z-10 text-white">
+              <span className="text-xs font-light">{products.length}</span>
+            </div>
           </Link>
-          <div className="flex items-center justify-center bg-[#ef7014] px-1 min-w-[16px] h-4 absolute rounded-full -top-1 -right-1 z-10 text-white">
-            <span className="text-xs font-light">{products.length}</span>
-          </div>
         </div>
         <div className="hidden md:block">
           <Link href="/favor" className="text-3xl justify-center items-center flex">
